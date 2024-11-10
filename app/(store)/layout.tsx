b8e88d0d50408a05ecca83355430f39b976ecfa8
SHAKIL AHMAD <learnershakil@gmail.com>
2024-11-10 06:35:49 +0530

    v0.0.1: Navbar Done!!

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Welcome to e-commerce store",
@@ -16,7 +17,10 @@ export default function RootLayout({
     <ClerkProvider dynamic>
     <html lang="en">
       <body>
+        <main>
+        <Header />
         {children}
+        </main>
       </body>
     </html>
     </ClerkProvider>
